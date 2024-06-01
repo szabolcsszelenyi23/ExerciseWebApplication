33bca33390a5a8db3808312640c99a6f57a8c16b
Szabolcs <szabolcs.szelenyi23@gmail.com>
2024-06-01 14:43:11 +0200

    Progress

--- ExerciseWebApplication1/Controllers/HomeController.cs
+++ ExerciseWebApplication1/Controllers/HomeController.cs
@@ -23,6 +23,21 @@ namespace ExerciseWebApplication1.Controllers
             return View();
         }
 
+        public IActionResult Expenses()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            return RedirectToAction("Index");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ ExerciseWebApplication1/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExerciseWebApplication1.Models
+{
+    public class Expense
+    {
+        public int ID { get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
